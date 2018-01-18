358b684a23ee53dc5eaaf1703c4ca92312ca8fba
Luis Alberto <lainiguez@softbuilder.com.ec>
2018-01-18 08:22:52 -0500

    Fix - index redirect

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /* Redirect browser */
-define('URL', 'http://crm-softbuilder.rhcloud.com/');
+define('URL', 'http://crm-vtiger-crm-softbuilder.b9ad.pro-us-east-1.openshiftapps.com/');
 header('Location: '.URL.'php/portal/index.php', TRUE, 302); 
 
 /* Make sure that code below does not get executed when we redirect. */
